0fbac7e52eb3a3f9732a6167b9016825e70a78b9
Quinn Harris <quinnharris0925@gmail.com>
2023-05-05 16:26:56 -0400

    forum_plus

--- static/scripts/data.js
+++ static/scripts/data.js
@@ -199,4 +199,228 @@ var defaultThreads = [
         }
     ]
 }
+
 ]
+var threads_plus = [
+    {
+        id: 1,
+        title: "Mcdonalds Poor Service",
+        author: "Aaron H",
+        address: "1800 J Mullard",
+        content: "I cannot beleive how rude they were when...",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Secret Menu Items Teir-list",
+        author: "Aaron",
+        address: "N/A",
+        content: "The best secret menu item by far is the...",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Authentic Italian Pizza",
+        author: "Sophia P",
+        address: "225 L'italiano Ave",
+        content: "If you're looking for a true taste of Italy...",
+        comments: [
+            {
+                author: "Luigi",
+                date: Date.now(),
+                content: "I totally agree, their pizza is amazing!"
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "Sushi Lovers Unite!",
+        author: "Samantha R",
+        address: "123 Main St",
+        content: "Sushi is the best cuisine in the world, and...",
+        comments: [
+            {
+                author: "Yuki",
+                date: Date.now(),
+                content: "I'm a sushi chef, and I can tell you that..."
+            },
+            {
+                author: "Derek",
+                date: Date.now(),
+                content: "I don't really like sushi, but my girlfriend does..."
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "BBQ Ribs and Chicken",
+        author: "Robert L",
+        address: "456 Grillin' Rd",
+        content: "If you're in the mood for some delicious BBQ...",
+        comments: [
+            {
+                author: "Billy",
+                date: Date.now(),
+                content: "I'm a BBQ fanatic, and I have to say that this place is top-notch!"
+            },
+            {
+                author: "Katie",
+                date: Date.now(),
+                content: "I've never really been into BBQ, but my friends love this place..."
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Vegan Delights",
+        author: "Olivia S",
+        address: "789 Leafy Ln",
+        content: "If you're a vegan looking for some tasty options...",
+        comments: [
+            {
+                author: "Tia",
+                date: Date.now(),
+                content: "I'm not even vegan, but I love this place!"
+            },
+            {
+                author: "Ava",
+                date: Date.now(),
+                content: "As a vegan, I appreciate having so many options here..."
+            }
+        ]
+    },
+    {
+        id: 7,
+        title: "Famous Burger Joint",
+        author: "William J",
+        address: "321 Burger Blvd",
+        content: "You haven't lived until you've tried the burgers at this place...",
+        comments: [
+            {
+                author: "Tony",
+                date: Date.now(),
+                content: "I've been coming here for years, and the burgers never disappoint!"
+            },
+            {
+                author: "Liam",
+                date: Date.now(),
+                content: "I don't know what all the fuss is about, the burgers here are just okay..."
+            }
+        ]
+    },{        id: 8,        title: "Authentic Mexican Tacos",        author: "Maria G",        address: "555 Taco St",        content: "If you're a fan of Mexican cuisine, you have to try the tacos at this place...",        comments: [            {                author: "Javier",                date: Date.now(),                content: "I'm from Mexico and I can confirm that these tacos are amazing!"            },            {                author: "Grace",                date: Date.now(),                content: "I'm not a fan of spicy food, but these tacos are delicious regardless!"            }        ]
+},
+{
+    id: 9,
+    title: "Gourmet Burgers and Fries",
+    author: "David S",
+    address: "123 Burger St",
+    content: "Looking for a burger joint that takes things up a notch? Look no further than this place...",
+    comments: [
+        {
+            author: "Mike",
+            date: Date.now(),
+            content: "The truffle fries alone are worth the trip, but the burgers are out of this world!"
+        },
+        {
+            author: "Maggie",
+            date: Date.now(),
+            content: "I love that they offer so many vegetarian options here, even the meat-eaters in my group were impressed!"
+        }
+    ]
+},
+{
+    id: 10,
+    title: "Japanese Ramen",
+    author: "Takumi",
+    address: "777 Noodle Ave",
+    content: "If you're a fan of Japanese cuisine, you can't leave town without trying this ramen joint...",
+    comments: [
+        {
+            author: "Yoshi",
+            date: Date.now(),
+            content: "As someone who grew up eating ramen in Japan, I can say that this place is legit!"
+        },
+        {
+            author: "Lauren",
+            date: Date.now(),
+            content: "The broth was a bit too salty for my taste, but the noodles were perfectly cooked!"
+        }
+    ]
+},
+{
+    id: 11,
+    title: "Healthy Smoothies and Juices",
+    author: "Emily K",
+    address: "999 Juice Blvd",
+    content: "Looking for a healthy alternative to your usual fast food fix? Check out this juice bar...",
+    comments: [
+        {
+            author: "Avery",
+            date: Date.now(),
+            content: "I'm not a big fan of fruits and veggies, but I can get behind these smoothies!"
+        },
+        {
+            author: "Isaac",
+            date: Date.now(),
+            content: "I appreciate that they offer add-ins like protein powder and chia seeds to make the drinks even more nutritious!"
+        }
+    ]
+},
+{
+    id: 12,
+    title: "All-You-Can-Eat Sushi",
+    author: "Kaitlyn W",
+    address: "444 Sushi Lane",
+    content: "If you're a sushi lover on a budget, you need to check out this all-you-can-eat joint...",
+    comments: [
+        {
+            author: "Jenny",
+            date: Date.now(),
+            content: "I was skeptical at first, but the quality of the sushi was surprisingly good!"
+        },
+        {
+            author: "Max",
+            date: Date.now(),
+            content: "I ate so much sushi that I had to be rolled out of the restaurant, but it was worth it!"
+        }
+    ]
+},
+{
+    id: 13,
+    title: "Cookout Was Amazing!!!",
+    author: "Lenny V.",
+    address: "9705 N Tryon St, Charlotte, NC 28262",
+    content: "Customer service was a 10/10 and the burgers were cooked to perfection...",
+    comments: [
+        {
+            author: "Jenny",
+            date: Date.now(),
+            content: "I was skeptical at first, but the quality of the sushi was surprisingly good!"
+        },
+        {
+            author: "Max",
+            date: Date.now(),
+            content: "I ate so much sushi that I had to be rolled out of the restaurant, but it was worth it!"
+        }
+    ]
+}
+
+]
+
